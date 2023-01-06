f38c9a8961b1fb22f7cfc046acc17b93b67ba32e
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2023-01-06 20:17:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "google" {
-  access_token = var.access_token
+  version = "~> 3.28.0"
+  client_id = var.client_id
+  client_secret = var.client_secret
   project     = "ite-project-370218"
   region      = "us-central1"
 }
